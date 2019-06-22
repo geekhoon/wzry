c94fce40e4cb5da3e61d74b66c3166d9650f3417
wpwing <wpwing@163.com>
2019-06-22 22:31:01 +0800

    6月22日整合结束

--- royal_web/src/main/java/com/bbs/controller/UserController.java
+++ royal_web/src/main/java/com/bbs/controller/UserController.java
@@ -6,28 +6,20 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< Updated upstream:royal_web/src/main/java/com/bbs/manage/controller/UserController.java
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-=======
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
->>>>>>> Stashed changes:royal_web/src/main/java/com/bbs/controller/UserController.java
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-<<<<<<< Updated upstream:royal_web/src/main/java/com/bbs/manage/controller/UserController.java
 import java.util.List;
-=======
-import java.net.URLEncoder;
 import java.util.UUID;
->>>>>>> Stashed changes:royal_web/src/main/java/com/bbs/controller/UserController.java
+
 
 @Controller
 @RequestMapping("/user")
